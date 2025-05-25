ea6eef2950929c33431a6b9f0afb7dcfd62fdecf
Rukshan-Vimukthi <rukshanse.info@gmail.com>
2025-05-25 22:59:20 +0530

    Temp commit

---
+++ src/main/java/com/example/inventorymanagementsystem/services/interfaces/ItemPreviewObserver.java
@@ -0,0 +1,7 @@
+package com.example.inventorymanagementsystem.services.interfaces;
+
+import com.example.inventorymanagementsystem.models.ItemDetail;
+
+public interface ItemPreviewObserver {
+    void update(ItemDetail itemDetail);
+}
